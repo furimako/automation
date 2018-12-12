32a8bc906c9f3b952c79415985a62dd1e6154f67
furimako <furimako@gmail.com>
2018-12-12 20:32:53 +0900

    add follow.js

--- js/twitter/follow.js
+++ js/twitter/follow.js
@@ -1,53 +1,53 @@
 const fs = require('fs')
 const puppeteer = require('puppeteer')
-const PASSWORD = JSON.parse( fs.readFileSync('./twitter/password.json', 'utf8') )
-const FOLLOW = JSON.parse( fs.readFileSync('./twitter/follow.json', 'utf8') )
+const logging = require('./../logging')
+const CONFIG = JSON.parse( fs.readFileSync('./config/twitter-config.json', 'utf8') )
+const PARAM = JSON.parse( fs.readFileSync('./js/twitter/twitter.json', 'utf8') )
 
-async function follow() {
-    await console.log('--- the process started ---')
-    await console.log(`NUM_OF_USERS: ${FOLLOW.numOfUsers}`)
-    await console.log(`NUM_OF_FOLLOWS_PER_USER: ${FOLLOW.numOfFollowsPerUser}`)
+module.exports = async function follow() {
+    await logging.info('the process started')
+    await logging.info(`NUM_OF_USERS: ${PARAM.numOfUsers}`)
+    await logging.info(`NUM_OF_FOLLOWS_PER_USER: ${PARAM.numOfFollowsPerUser}`)
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: process.env.NODE_ENV === 'production',
         slowMo: 10
     })
     const page = await browser.newPage()
     
     // Login screen
     await page.goto('https://twitter.com/login')
-    await page.type('input.js-username-field', PASSWORD.address)
-    await page.type('input.js-password-field', PASSWORD.password)
+    await page.type('input.js-username-field', CONFIG.address)
+    await page.type('input.js-password-field', CONFIG.password)
     await page.click('button[type="submit"]')
-    await console.log('[Login] finished')
+    await logging.info('login finished')
     
     // Get target URLs
-    await page.goto('https://twitter.com/search?f=users&vertical=default&q=' + FOLLOW.keyword + '&src=typd')
+    await page.goto('https://twitter.com/search?f=users&vertical=default&q=' + PARAM.keyword + '&src=typd')
     const LINK_SELECTOR = '.GridTimeline-items > .Grid > .Grid-cell .fullname'
     await page.waitForSelector(LINK_SELECTOR)
     const targetURLs = await page.evaluate(selector => {
         const elementList = document.querySelectorAll(selector)
         return Array.from(elementList, element => element.href)
     }, LINK_SELECTOR)
-    await console.log('[Get target URL] finished')
-    await console.log(`target URLs:\n${targetURLs.join('\n')}`)
+    await logging.info('acquired all target URLs')
+    await logging.info(`target URLs:\n${targetURLs.join('\n')}`)
     
     // Follow
-    if (targetURLs.length < FOLLOW.numOfUsers) {
+    if (targetURLs.length < PARAM.numOfUsers) {
         // should not be here
         process.exit(1)
     }
     const followButtonSelector = (i, j) => {
         return `.GridTimeline-items > .Grid:nth-child(${i}) > .Grid-cell:nth-child(${j}) .EdgeButton:nth-child(1)`
     }
-    for (let userID = 1; userID <= FOLLOW.numOfUsers; userID++) {
+    for (let userID = 1; userID <= PARAM.numOfUsers; userID++) {
         await page.goto(targetURLs[userID] + '/followers')
         let followCount = 0
         let completeFlag = false
         for (let i = 1; i <= 3; i++) {
             for (let j = 1; j <= 6; j++) {
                 followCount++
-                if (followCount > FOLLOW.numOfFollowsPerUser) {
-                    await console.log(`[follow] finished (user ID: ${userID})`)
+                if (followCount > PARAM.numOfFollowsPerUser) {
                     completeFlag = true
                     break
                 }
@@ -62,7 +62,7 @@ async function follow() {
                 
                 await page.waitFor( followButtonSelector(i, j) )
                 await page.click( followButtonSelector(i, j) )
-                await console.log(`follow (user ID: ${userID}, follow count: ${followCount})`)
+                await logging.info(`followed (user ID: ${userID}, follow count: ${followCount})`)
             }
             if (completeFlag) {
                 break
@@ -70,9 +70,9 @@ async function follow() {
         }
     }
     
-    await console.log('--- the process finished ---')
-    // await browser.close()
+    await logging.info('the process finished')
+    if (process.env.NODE_ENV === 'production') {
+        await browser.close()
+    }
     return
 }
-
-follow()
