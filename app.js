6dcc92c6f8805dc40e286bd7143af80efca1408d
furimako <furimako@gmail.com>
2018-12-23 10:31:46 +0900

    add crontab settings

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+const logging = require('./js/logging')
+const tweet = require('./js/twitter/tweet')
 const follow = require('./js/twitter/follow')
 
-follow()
+// command: tweet / follow
+const command = process.argv[2]
+
+if (command === 'tweet') {
+    logging.info('starting tweet')
+    tweet()
+    process.exit(0)
+}
+
+if (command === 'follow') {
+    logging.info('starting follow')
+    follow()
+    process.exit(0)
+}
+
+// should not be here
+logging.err('command should be wrong')
+process.exit(1)
---
+++ configs/crontab/crontab.sh
@@ -0,0 +1,20 @@
+
+#!/bin/sh
+
+# 
+# Ubuntu 18.04.1
+# 
+
+# $1 = "tweet" / "follow"
+action=$1
+
+echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] confirm crontab list"
+crontab -l
+
+echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] rotate log files"
+cd ~/automation/logs
+mv ${action}.log archive/${action}_`date +%Y%m%d%H%M%S`.log
+
+echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] start app (${action})"
+cd ~/automation
+npm run ${action}
