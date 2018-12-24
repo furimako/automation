78a585cfc81af6b057deecfd10f9194271311177
furimako <furimako@gmail.com>
2018-12-25 08:55:54 +0900

    create Page class

--- js/twitter/follow.js
+++ js/twitter/follow.js
@@ -1,99 +1,87 @@
 const fs = require('fs')
-const puppeteer = require('puppeteer')
+const Page = require('./page')
 const logging = require('./../logging')
 
-const config = JSON.parse(fs.readFileSync('./configs/twitter-config.json', 'utf8'))
 const param = JSON.parse(fs.readFileSync('./js/twitter/twitter.json', 'utf8'))
 
 module.exports = function follow() {
     return new Promise(async (resolve) => {
         await logging.info(`numOfFollows: ${param.numOfFollows}`)
-        
-        const browser = await puppeteer.launch({
-            headless: process.env.NODE_ENV === 'production',
-            slowMo: 10
-        })
-        const page = await browser.newPage()
+        const page = new PageForFollow()
+        await page.init(process.env.NODE_ENV)
         
         // Login screen
-        await login(page)
+        await page.login()
         await logging.info('login finished')
         
         // Get target URLs
-        const targetURLs = await getTargetURLs(page)
+        const targetURLs = await page.getTargetURLsWithKeyword()
         await logging.info('acquired all target URLs')
         await logging.info(`target URLs:\n${targetURLs.join('\n')}`)
         
         // Follow
-        const result = await executeFollow(page, targetURLs)
+        const result = await page.executeFollow(targetURLs)
         
         if (process.env.NODE_ENV === 'production') {
-            await browser.close()
+            await page.close()
         }
         resolve(result)
     })
 }
 
 
-async function login(page) {
-    await page.goto('https://twitter.com/login')
-    await page.type('input.js-username-field', config.address)
-    await page.type('input.js-password-field', config.password)
-    await page.click('button[type="submit"]')
-}
-
-
-async function getTargetURLs(page) {
-    await page.goto(`https://twitter.com/search?f=users&vertical=default&q=${param.keyword}&src=typd`)
-    
-    const linkSelector = '.GridTimeline-items > .Grid > .Grid-cell .fullname'
-    await page.waitForSelector(linkSelector)
-    
-    return page.evaluate((selector) => {
-        const elementList = document.querySelectorAll(selector)
-        return Array.from(elementList, element => element.href)
-    }, linkSelector)
-}
-
-
-function executeFollow(page, targetURLs) {
-    return new Promise(async (resolve, reject) => {
-        const followButtonSelector = (i, j) => `.GridTimeline-items > .Grid:nth-child(${i}) > .Grid-cell:nth-child(${j}) .EdgeButton:nth-child(1)`
-        const result = {}
+class PageForFollow extends Page {
+    async getTargetURLsWithKeyword() {
+        await this.page.goto(`https://twitter.com/search?f=users&vertical=default&q=${param.keyword}&src=typd`)
         
-        let followCount = 0
-        for (let userID = 1; userID <= 18; userID += 1) {
-            result[targetURLs[userID]] = 0
-            await page.goto(`${targetURLs[userID]}/followers`)
+        const linkSelector = '.GridTimeline-items > .Grid > .Grid-cell .fullname'
+        await this.page.waitForSelector(linkSelector)
+        
+        return this.page.evaluate((selector) => {
+            const elementList = document.querySelectorAll(selector)
+            return Array.from(elementList, element => element.href)
+        }, linkSelector)
+    }
+    
+    executeFollow(targetURLs) {
+        return new Promise(async (resolve, reject) => {
+            const followButtonSelector = (i, j) => `.GridTimeline-items > .Grid:nth-child(${i}) > .Grid-cell:nth-child(${j}) .EdgeButton:nth-child(1)`
+            const result = {}
             
-            for (let i = 1; i <= 3; i += 1) {
-                for (let j = 1; j <= 6; j += 1) {
-                    await page.waitForSelector(followButtonSelector(i, j))
-                    await page.evaluate((selector) => {
-                        if (document.querySelectorAll(selector).length !== 1) {
-                            // should not be here
-                            reject(new Error('some change has been made in Twitter'))
-                        }
-                    }, followButtonSelector(i, j))
-                    
-                    await page.waitFor(followButtonSelector(i, j))
-                    try {
-                        await page.click(followButtonSelector(i, j))
-                        result[targetURLs[userID]] += 1
-                        followCount += 1
-                        await logging.info(`followed (userID: ${userID}, count: ${followCount})`)
+            let followCount = 0
+            for (let userID = 1; userID <= 18; userID += 1) {
+                result[targetURLs[userID]] = 0
+                await this.page.goto(`${targetURLs[userID]}/followers`)
+                
+                for (let i = 1; i <= 3; i += 1) {
+                    for (let j = 1; j <= 6; j += 1) {
+                        await this.page.waitForSelector(followButtonSelector(i, j))
+                        await this.page.evaluate((selector) => {
+                            if (document.querySelectorAll(selector).length !== 1) {
+                                // should not be here
+                                reject(new Error('some change has been made in Twitter'))
+                            }
+                        }, followButtonSelector(i, j))
                         
-                        if (followCount === param.numOfFollows) {
-                            resolve(result)
-                            return
+                        await this.page.waitFor(followButtonSelector(i, j))
+                        try {
+                            await this.page.click(followButtonSelector(i, j))
+                            result[targetURLs[userID]] += 1
+                            followCount += 1
+                            await logging.info(`followed (userID: ${userID}, count: ${followCount})`)
+                            
+                            if (followCount === param.numOfFollows) {
+                                resolve(result)
+                                return
+                            }
+                        } catch (err) {
+                            await logging.info('the account might be already followed')
+                            continue
                         }
-                    } catch (err) {
-                        await logging.info('the account might be already followed')
-                        continue
                     }
                 }
             }
-        }
-        reject(new Error('numOfFollows might be too large'))
-    })
+            reject(new Error('numOfFollows might be too large'))
+        })
+    }
 }
---
+++ js/twitter/page.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+const config = JSON.parse(fs.readFileSync('./configs/twitter-config.json', 'utf8'))
+
+module.exports = class Page {
+    async init(env) {
+        this.browser = await puppeteer.launch({
+            headless: env === 'production',
+            slowMo: 10
+        })
+        this.page = await this.browser.newPage()
+    }
+
+    async login() {
+        await this.page.goto('https://twitter.com/login')
+        await this.page.type('input.js-username-field', config.address)
+        await this.page.type('input.js-password-field', config.password)
+        await this.page.click('button[type="submit"]')
+    }
+    
+    async close() {
+        await this.browser.close()
+    }
+}
