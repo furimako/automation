ea73bf2a656bc39e95428c239641082243d9377c
furimako <furimako@gmail.com>
2018-12-24 20:00:43 +0900

    update message contents of mailer.js

--- js/mailer.js
+++ js/mailer.js
@@ -19,8 +19,8 @@ module.exports = {
             }
 
             console.log('--- sending mail ---')
-            console.log(`body: ${body}`)
-            console.log(`text: ${text}`)
+            console.log(`[body]\n${body}\n`)
+            console.log(`[text]\n${text}`)
         })
     }
 }
