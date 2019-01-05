4fc124d975775ebccd39c4e3d79a529ad016a5fc
furimako <furimako@gmail.com>
2019-01-05 11:31:04 +0900

    update logging

--- app.js
+++ app.js
@@ -26,11 +26,15 @@ async function execute() {
         await logging.info('finished login')
         
         const targetURLs = await follow.getTargetURLsWithKeyword(keyword)
-        await logging.info(`finished getting targetURLs\n<<targetURLs>>\n${targetURLs.join('\n')}`)
+        await logging.info('finished getting targetURLs')
+        await logging.info(`targetURLs are shown below\n${targetURLs.join('\n')}`)
         
         const result = await follow.clickFollowButtons(targetURLs)
+        const totalCount = Object.values(result).map(v => v.success).reduce((total, v) => total + v)
         const resultStr = Object.keys(result).map(key => `URL: ${key}, follow: ${result[key].success}, fail: ${result[key].fail}`).join('\n')
-        await logging.info(`finished clicking follow buttons\n<<result>>\n${resultStr}`)
+        await logging.info('finished clicking follow buttons')
+        await logging.info(`total count: ${totalCount}`)
+        await logging.info(`details are shown below\n${resultStr}`)
         
         if (env === 'production') {
             await follow.close()
