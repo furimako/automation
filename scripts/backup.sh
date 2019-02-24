cf15381d4e6ae100c91592376b512740d0c9cb1c
furimako <furimako@gmail.com>
2019-02-24 17:11:46 +0900

    change the server of automation

---
+++ scripts/backup.sh
@@ -0,0 +1,13 @@
+
+#!/bin/sh
+
+#
+# macOS
+#
+
+local_path="/Users/furimako/Documents/Dropbox/makoto/backup/automation/$(date +%Y%m%d)"
+mkdir $local_path
+
+sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@automation.furimako.com:~/automation/logs/* $local_path
+
+echo "created backup (path: ${local_path})"
