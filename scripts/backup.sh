63eadb0215a70d2bc333de43dbfa1c0b5540c9e9
furimako <furimako@gmail.com>
2019-03-10 16:12:56 +0900

    change the domain

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -8,6 +8,6 @@
 local_path="/Users/furimako/Documents/Dropbox/makoto/backup/automation/$(date +%Y%m%d)"
 mkdir $local_path
 
-sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@furimako.com:~/automation/logs/* $local_path
+sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@automation.furimako.com:~/automation/logs/* $local_path
 
 echo "created backup (path: ${local_path})"
