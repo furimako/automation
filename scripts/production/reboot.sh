abb132fcff54afeb4037c21d20faaf2b4de6c3d2
furimako <furimako@gmail.com>
2019-03-15 12:30:41 +0900

    update script

--- scripts/production/reboot.sh
+++ scripts/production/reboot.sh
@@ -4,7 +4,7 @@
 # Ubuntu
 #
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start reboot.sh"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] START reboot.sh"
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] confirm crontab list"
 crontab -l
@@ -13,6 +13,9 @@ echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] rotate log files"
 cd ~/automation/logs
 mv app.log archives/app_$(date +'%Y%m%d%H%M%S').log
 
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] apt update"
 sudo apt update
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] apt -y dist-upgrade"
 sudo apt -y dist-upgrade
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start reboot"
 sudo shutdown -r now
