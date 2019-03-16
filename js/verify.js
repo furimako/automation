c785a26e962f22b3c80fd298e6281a89d1a52f36
furimako <furimako@gmail.com>
2019-03-16 21:14:41 +0900

    fix bug

--- js/verify.js
+++ js/verify.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
+const Base = require('./base')
 
 const config = JSON.parse(fs.readFileSync('./configs/twitter-config.json', 'utf8'))
 
-module.exports = class Browser {
+module.exports = class Verify extends Base {
     async execute() {
         await this.page.type('input#challenge_response', config.phoneNumber)
         await this.page.click('input#email_challenge_submit')
