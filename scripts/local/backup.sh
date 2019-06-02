4c2e52186703336a6d67243f242a0897f1a88f9d
furimako <furimako@gmail.com>
2019-06-02 09:51:32 +0900

    update path for backup

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -5,7 +5,7 @@
 # macOS
 #
 
-local_path="/Users/furimako/Documents/Dropbox/makoto/backup/automation/$(date +%Y%m%d)"
+local_path="/Users/furimako/Documents/Dropbox/makoto/others/backups/automation/$(date +%Y%m%d)"
 mkdir $local_path
 
 sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@automation.furimako.com:~/automation/logs/* $local_path
