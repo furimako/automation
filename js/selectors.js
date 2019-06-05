dd19ee1076b070c4f654bf7dbf44fcc71fc2cd12
furimako <furimako@gmail.com>
2019-06-06 07:19:47 +0900

    update unfollow selector

--- js/selectors.js
+++ js/selectors.js
@@ -14,5 +14,5 @@ module.exports = {
     // Follow or Follower List
     protectedIcon: i => `div.css-1dbjc4n > div:nth-child(${i}) > div.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll > div[role="button"] > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a > div > div:nth-child(1) > div:nth-child(2)`,
     followButton: i => `div.css-1dbjc4n > div:nth-child(${i}) > div.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll > div[role="button"] > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div[role="button"]`,
-    yesToConfirmation: 'div[role="button"].css-18t94o4.css-1dbjc4n.r-urgr8i.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-1w2pmg.r-1x0uki6.r-1vuscfd.r-1dhvaqw.r-1fneopy.r-o7ynqc.r-6416eg.r-lrvibr'
+    yesToConfirmation: 'div[role="button"].css-18t94o4.css-1dbjc4n.r-urgr8i.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-16y2uox.r-1w2pmg.r-1vuscfd.r-1dhvaqw.r-1fneopy.r-o7ynqc.r-6416eg.r-lrvibr'
 }
