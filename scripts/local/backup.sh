0e1da11eb43508c61a79b626fff0884d9d7e93a3
furimako <furimako@gmail.com>
2019-06-15 10:16:25 +0900

    update backup script

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -5,9 +5,9 @@
 # macOS
 #
 
-local_path="/Users/furimako/Documents/Dropbox/makoto/others/backups/automation/$(date +%Y%m%d)"
+local_path="/Users/furimako/Documents/Dropbox/makoto/5-others/backups/automation/$(date +%Y%m%d)"
 mkdir $local_path
 
-sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@automation.furimako.com:~/automation/logs/* $local_path
+sudo scp -i /Users/furimako/.ssh/id_rsa -r furimako@automation.furimako.com:~/automation/logs/* $local_path
 
 echo "created backup (path: ${local_path})"
