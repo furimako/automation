b715c2192daf3ee9ca84c6a3c38584e2e5b8510c
furimako <furimako@gmail.com>
2020-06-14 08:17:17 +0900

    add another Twitter account

--- scripts/production/app.sh
+++ scripts/production/app.sh
@@ -8,8 +8,9 @@
 command=$1
 count=$2
 keyword=$3
+account=$4
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start app.sh (command: ${command})"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start app.sh (command: ${command}, count: ${count}, keyword: ${keyword}, account: ${account})"
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] confirm crontab list"
 crontab -l
@@ -19,6 +20,6 @@ seconds=$(($RANDOM*60*15/32768))
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] sleep ${seconds}s"
 sleep ${seconds}
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start app (command: ${command})"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start app (command: ${command}, count: ${count}, keyword: ${keyword}, account: ${account})"
 cd ~/automation
-NODE_ENV=production node app.js ${command} ${count} ${keyword}
+NODE_ENV=production node app.js ${command} ${count} ${keyword} ${account}
