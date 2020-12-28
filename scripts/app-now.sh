6100c22c95967b24010a3be5e25e5e32cbb645ab
furimako <furimako@gmail.com>
2020-12-28 18:24:55 +0900

    add quick option to app-now.sh

--- scripts/app-now.sh
+++ scripts/app-now.sh
@@ -9,6 +9,7 @@ command=$1
 account=$2
 count=$3
 keyword=$4
+quick=$5
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start app-now.sh (command: ${command}, account: ${account}, count: ${count}, keyword: ${keyword})"
-NODE_ENV=production node app.js ${command} ${account} ${count} ${keyword}
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start app-now.sh (command: ${command}, account: ${account}, count: ${count}, keyword: ${keyword}, quick: ${quick})"
+NODE_ENV=production node app.js ${command} ${account} ${count} ${keyword} ${quick}
