abf262b6a866ea1c5cfbdca74cf41d1a6b38984c
furimako <furimako@gmail.com>
2021-02-28 10:29:07 +0900

    update selector for confirmation

--- js/selectors.js
+++ js/selectors.js
@@ -17,7 +17,7 @@ module.exports = {
     userDescription: 'main > div > div > div > div > div > div:nth-child(2) > div > div > div:nth-child(1) > div > div:nth-child(3) > div > div > span',
     
     // Confirmation
-    yesToConfirmation: 'div[role="button"].css-18t94o4.css-1dbjc4n.r-urgr8i.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-16y2uox.r-1w2pmg.r-1vuscfd.r-1dhvaqw.r-1fneopy.r-o7ynqc.r-6416eg.r-lrvibr',
+    yesToConfirmation: '#layers > div:nth-child(2) > div > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2)',
     
     // Follow & Follower List
     userName: (i) => `${followerListBase(i)} > div:nth-child(1) > div:nth-child(1) > a > div > div:nth-child(2) > div > span`,
