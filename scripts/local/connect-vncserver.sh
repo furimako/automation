601851f0eb85f88bd4e55df1dea73dc105fa401b
furimako <furimako@gmail.com>
2021-06-07 20:37:16 +0900

    change DigitalOcean to AWS Lightsail

--- scripts/local/connect-vncserver.sh
+++ scripts/local/connect-vncserver.sh
@@ -4,4 +4,4 @@
 # macOS
 #
 
-ssh -i ~/.ssh/LightsailDefaultKey-ap-northeast-1.pem -L 5901:127.0.0.1:5901 -C -N -l ubuntu 35.72.5.42
+ssh -i ~/.ssh/LightsailDefaultKey-ap-northeast-1.pem -L 5901:127.0.0.1:5901 -C -N -l ubuntu automation.furimako.com
