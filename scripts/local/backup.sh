150bdc941dd94221f392b809a784b70e3165937f
furimako <furimako@gmail.com>
2021-07-20 13:04:41 +0900

    update path of the script for backup

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -5,7 +5,7 @@
 # macOS
 #
 
-local_path="/Users/furimako/Documents/Dropbox/makoto/5-others/backups/automation/$(date +%Y%m%d)"
+local_path="/Users/furimako/Documents/Dropbox/makoto/5_others/backups/automation/$(date +%Y%m%d)"
 mkdir $local_path
 
 sudo scp -i ~/.ssh/LightsailDefaultKey-ap-northeast-1.pem -r ubuntu@automation.furimako.com:~/automation/logs/* $local_path
